72f91a165fbb1719a585e6cf679702cf2b5b1be2
grantbaker <grant.baker@colorado.edu>
2017-03-05 23:24:20 -0700

    better sync script

--- sync.sh
+++ sync.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 git push origin master
-cp index.html /keybase/public/grantbaker/
-rsync index.html grba6061@elra-02.cs.colorado.edu:~/.www/
+#cp !(sync.sh) /keybase/public/grantbaker/
+#rsync !(sync.sh) grba6061@elra-02.cs.colorado.edu:~/.www/
+
+#find ./ -type f -maxdepth 1 ! -name sync.sh -exec cp -t /keybase/public/grantbaker/ {} +
+
+rsync -v ./* /keybase/public/grantbaker/ --exclude sync.sh --exclude .git
+rsync -v ./* grba6061@elra-02.cs.colorado.edu:~/.www/ --exclude sync.sh --exclude .git
