f9c2ef4dfe05d4e6ccba0a6cbbcd88dfcee15301
grantbaker <grant.baker@colorado.edu>
2018-08-20 16:18:46 -0600

    Update for BBQ

--- bingo-card-generator/bingo-card.js
+++ bingo-card-generator/bingo-card.js
@@ -1,7 +1,7 @@
 window.onload = initAll;
 
 // do I even know javascript?
-var hardcodedStrings = ["\"Welcome to ___ on this most excellent of all ___\"",
+var banquetStrings = ["\"Welcome to ___ on this most excellent of all ___\"",
 "\"___-ness of ___\"",
 "\"Ambitious without being competitive\"",
 "The great EHP experiment",
@@ -40,6 +40,42 @@ var hardcodedStrings = ["\"Welcome to ___ on this most excellent of all ___\"",
 "\"Wasting valuable tuition dollars\"",
 "\"Beat the snot out of...\""
 ]
+
+var bbqStrings = ["\"Welcome to ___ on this most excellent of all ___\"",
+"\"___-ness of ___\"",
+"\"Ambitious without being competitive\"",
+"The great EHP experiment",
+"The greatest resource engineering students have is their peers",
+"\"And there you have that\"",
+"\"Deeply deeply\"",
+"\"What does it mean to ___?\"",
+"\"And again\" or \"and another thing\"",
+"Integration over small life events/sum of infinite infinitesimally small moments",
+"Blind chance",
+"\"Being about ___\"",
+"Westworld",
+"Harry Potter",
+"Lord of the Rings",
+"\"For those of you...\"",
+"Mentions conflict with Housing",
+"Any Greek word, other than &#913&#915&#921&#927&#925 &#931&#922&#933&#914&#913&#923&#927&#925",
+"HFFM",
+"Mentions 10th year of the program",
+"Draws similarity between himself and students' parents"
+"Emotional bandwidth or emotional units",
+"Speaks highly of 3 or more EHP alumni",
+"Goes over what other EHP events we do",
+"\"Failing better\"",
+"\"SO, ...\" where \"so\" is 10x louder than the rest",
+"\"___ of ___ fame\"",
+"\"We don't talk about statistics, but... (insert statistic here)\"",
+"Uses physics/math metaphor to explain philosophical concept",
+"2 or more random/pointless EHP statistics or records",
+"\"I sincerely hope\"",
+"Any swear word",
+]
+
+var hardcodedStrings = bbqStrings.slice()
 
 var usedNums = new Array(hardcodedStrings.length);
 
